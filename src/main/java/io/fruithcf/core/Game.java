9268672a1a907faa8e900e763d821b53a8b4eda1
VawkeIO <io.vawke@gmail.com>
2016-10-21 23:32:59 +0200

    Init

---
+++ src/main/java/io/fruithcf/core/Game.java
@@ -0,0 +1,17 @@
+package io.fruithcf.core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Giovanni on 21-10-2016.
+ * <p>
+ * This file is part of the FruitHCF project.
+ * Copyright (c) 2016 FruitHCF;www.vawke.io / development@vawke.io
+ */
+public class Game extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+    }
+}
